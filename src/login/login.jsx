538a736d727897dca4a81b19a91e163afc8f6221
ethan <ethan0@byu.edu>
2025-02-15 23:25:21 -0700

    login and play .jsx

--- src/login/login.jsx
+++ src/login/login.jsx
@@ -2,8 +2,35 @@ import React from "react";
 
 export function Login() {
   return (
-    <main className="container-fluid bg-secondary text-center">
-      <div>login displayed here</div>
+    <main className="bg-light text-dark text-center align-items-center d-flex flex-column justify-content-center align-items-center">
+      <h1 className="title">LIGHTS OUT</h1>
+      <h1 className="title">EVOLUTION</h1>
+      <p className="apiCall">3rd party service call Heckler placeholder</p>
+
+      <form method="get" action="play.html">
+        <div className="input-group mb-3">
+          <span className="bar-text input-group-text">@ </span>
+          <input
+            className="bar-text form-control"
+            type="text"
+            placeholder="your@email.com"
+          />
+        </div>
+        <div className="input-group mb-3">
+          <span className="bar-text input-group-text">🔒 </span>
+          <input
+            className="bar-text form-control"
+            type="password"
+            placeholder="password"
+          />
+        </div>
+        <button className="buttons btn btn-primary button1" type="submit">
+          Login
+        </button>
+        <button className="buttons btn btn-secondary button2" type="submit">
+          Register
+        </button>
+      </form>
     </main>
   );
 }
--- src/play/play.jsx
+++ src/play/play.jsx
@@ -1,9 +1,145 @@
 import React from "react";
-
+import "./play.css";
 export function Play() {
   return (
-    <main className="container-fluid bg-secondary text-center">
-      <div>play displayed here</div>
+    <main className="bg-secondary bg-light text-dark text-center align-items-center d-flex flex-column justify-content-center align-items-center">
+      <div className="player">
+        <p className="h2"> Mystery player</p>
+        <ul className="alerts" role="alert">
+          <div className="alert alert-primary game-start">
+            Andy has started the game
+          </div>
+          <div className="alert alert-warning level-complete">
+            John has completed level 2!
+          </div>
+          <div className="alert alert-success game-complete">
+            John scored 73 points!!!
+          </div>
+        </ul>
+      </div>
+
+      <div className="game-stats">
+        <span className="game-info">
+          <label className="label" htmlFor="health">
+            ❤️
+          </label>
+          <input
+            className="game-text"
+            type="text"
+            id="health"
+            value="--"
+            readOnly
+          />
+        </span>
+        <span className="game-info">
+          <label className="label" htmlFor="score">
+            Score:
+          </label>
+          <input
+            className="game-text"
+            type="text"
+            id="score"
+            value="--"
+            readOnly
+          />
+        </span>
+      </div>
+
+      <table className="game-table ">
+        <tbody>
+          <tr className="game-row">
+            <td>
+              <button className="nolight"></button>
+            </td>
+            <td>
+              <button className="light"></button>
+            </td>
+            <td>
+              <button className="nolight"></button>
+            </td>
+            <td>
+              <button className="light"></button>
+            </td>
+            <td>
+              <button className="nolight"></button>
+            </td>
+          </tr>
+          <tr>
+            <td>
+              <button className="light"></button>
+            </td>
+            <td>
+              <button className="nolight"></button>
+            </td>
+            <td>
+              <button className="light"></button>
+            </td>
+            <td>
+              <button className="light"></button>
+            </td>
+            <td>
+              <button className="nolight"></button>
+            </td>
+          </tr>
+          <tr>
+            <td>
+              <button className="nolight"></button>
+            </td>
+            <td>
+              <button className="light"></button>
+            </td>
+            <td>
+              <button className="nolight"></button>
+            </td>
+            <td>
+              <button className="light"></button>
+            </td>
+            <td>
+              <button className="light"></button>
+            </td>
+          </tr>
+          <tr>
+            <td>
+              <button className="nolight"></button>
+            </td>
+            <td>
+              <button className="light"></button>
+            </td>
+            <td>
+              <button className="light"></button>
+            </td>
+            <td>
+              <button className="nolight"></button>
+            </td>
+            <td>
+              <button className="light"></button>
+            </td>
+          </tr>
+          <tr>
+            <td>
+              <button className="light"></button>
+            </td>
+            <td>
+              <button className="light"></button>
+            </td>
+            <td>
+              <button className="nolight"></button>
+            </td>
+            <td>
+              <button className="light"></button>
+            </td>
+            <td>
+              <button className="light"></button>
+            </td>
+          </tr>
+        </tbody>
+      </table>
+
+      <div>
+        <button type="button" className="btn btn-outline-danger">
+          Give up
+        </button>
+      </div>
     </main>
   );
 }
