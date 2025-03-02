34c3c5d556a3a0adcb55fce780c15b5dacf8b63a
ethan <ethan0@byu.edu>
2025-03-01 20:58:08 -0700

    Started game notifications

---
+++ src/play/notification.jsx
@@ -0,0 +1,65 @@
+import React from "react";
+
+import { GameEvent, GameNotifier } from "./notifier";
+
+export function Notification(props) {
+  const userName = props.userName;
+
+  const [events, setEvents] = React.useState([]);
+
+  React.useEffect(() => {
+    GameNotifier.addHandler(handleGameEvent);
+    return () => {
+      GameNotifier.removeHandler(handleGameEvent);
+    };
+  }, []);
+
+  function handleGameEvent(event) {
+    setEvents((events) => {
+      let newEvents = [event, ...prevEvents];
+      if (newEvents.length > 10) {
+        newEvents = newEvents.slice(0, 10);
+      }
+      return newEvents;
+    });
+  }
+
+  function createMessageArray() {
+    const messageArray = [];
+    for (const [i, event] of events.entries()) {
+      let message = "unknown";
+      if (event.type === GameEvent.End) {
+        message = `scored ${event.value.score}`;
+      } else if (event.type === GameEvent.Start) {
+        message = `started a new game`;
+      } else if (event.type === GameEvent.System) {
+        message = event.value.msg;
+      }
+
+      messageArray.push(
+        <div key={i} className="event">
+          <span className={"player-event"}>{event.from.split("@")[0]}</span>
+          {message}
+        </div>
+      );
+    }
+    return messageArray;
+  }
+
+  return (
+    <div className="player">
+      <p className="h2"> {userName}</p>
+      <ul className="alerts" role="alert">
+        <div className="alert alert-primary game-start">
+          Andy has started the game
+        </div>
+        <div className="alert alert-warning level-complete">
+          John has completed level 2!
+        </div>
+        <div className="alert alert-success game-complete">
+          John scored 73 points!!!
+        </div>
+      </ul>
+    </div>
+  );
+}
---
+++ src/play/notifier.js
@@ -0,0 +1,52 @@
+const GameEvent = {
+  System: "system",
+  End: "gameEnd",
+  Start: "gameStart",
+};
+
+class EventMessage {
+  constructor(from, type, value) {
+    this.from = from;
+    this.type = type;
+    this.value = value;
+  }
+}
+
+class GameEventNotifier {
+  events = [];
+  handlers = [];
+
+  constructor() {
+    setInterval(() => {
+      const score = Math.floor(Math.random() * 100);
+      const userName = "Andy";
+      this.broadcast(userName, GameEvent.End, {
+        name: userName,
+        score: score,
+        date: date,
+      });
+    }, 5000);
+  }
+
+  broadcastEvent(from, type, value) {
+    const event = new EventMessage(from, type, value);
+    this.receiveEvent(event);
+  }
+
+  addHandler(handler) {
+    this.handlers.push(handler);
+  }
+
+  removeHandler(handler) {
+    this.handlers.filter((h) => h !== handler);
+  }
+
+  receiveEvent(event) {
+    this.events.push(event);
+    this.handlers.forEach((handler) => handler(event));
+  }
+
+  //   notifyAll(event) {
+  //     this.listeners.forEach((listener) => listener(event));
+  //   }
+}
