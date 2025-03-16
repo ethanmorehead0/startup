2deb946b2a7ebdba4dd2b45e8be8c2743eb45e26
ethan <ethan0@byu.edu>
2025-03-15 21:42:22 -0600

    api call to 3rd party service

--- src/login/login.jsx
+++ src/login/login.jsx
@@ -5,13 +5,25 @@ import { LogedIn } from "./logedIn";
 import { AuthState } from "./authState";
 
 export function Login({ userName, authState, onAuthChange }) {
+  const [setup, setSetup] = React.useState();
+  const [punchline, setPunchline] = React.useState();
+  React.useEffect(() => {
+    fetch("https://official-joke-api.appspot.com/jokes/random")
+      .then((response) => response.json())
+      .then((data) => {
+        setSetup(data.setup);
+        setPunchline(data.punchline);
+      });
+  }, []);
+
   return (
     <main className="bg-light text-dark text-center align-items-center d-flex flex-column justify-content-center align-items-center">
       {authState !== AuthState.Unknown && (
         <div>
           <h1 className="title">LIGHTS OUT</h1>
           <h1 className="title">EVOLUTION</h1>
-          <p className="apiCall">3rd party service call Heckler placeholder</p>
+          <p className="apiCall">{setup}</p>
+          <p className="apiCall">{punchline}</p>
         </div>
       )}
       <div>
